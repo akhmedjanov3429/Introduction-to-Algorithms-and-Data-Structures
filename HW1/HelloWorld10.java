1f6709d161715b75c8d261e96698c38fe0a8fdbb
Sardor Akhmedjonov <68418635+akhmedjanov3429@users.noreply.github.com>
2023-01-24 11:43:16 +0100

    Add files via upload

---
+++ HW1/HelloWorld10.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class HelloWorld10 {
+    public static void main(String[] args) {
+        
+        // Task: print "Hello, World" 10 times, each time ends with a new-line.
+        for (int i = 0; i < 10; i++) {
+        System.out.println("Hello, World");
+        }
+        // Hint: See "program 1.1.1" in Textbook and use 'System.out.println'.
+        
+        // Fill your code here
+        
+        // Complete the code here, see README on course website for problem description and instructions.
+
+
+
+
+    }
+}
